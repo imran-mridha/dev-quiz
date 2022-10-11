5abd24425bf00c0f8fa8fc34d9d673fa4ff4790c
MD IMRAN MRIDHA <mdimranmridha.pro@gmail.com>
2022-10-11 23:32:13 +0600

    Contact page Update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,12 +5,10 @@ import reader from '../../assets/contact.json'
 const Contact = () => {
   return (
     <div>
-      <h1>This Is Contact</h1>
-      <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 text-gray-100">
+      <h2 className="text-4xl font-semibold text-white text-center mt-10 lg:my-10 leading-tight lg:text-5xl">Let's <span className='font-semibold text-violet-500'>talk!</span> </h2>
+      <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16  text-gray-100">
         <div className="">
           <div className="">
-            <h2 className="text-4xl font-bold leading-tight lg:text-5xl">Let's talk!</h2>
-            <div className="text-gray-400">Vivamus in nisl metus? Phasellus.</div>
           </div>
           <Lottie animationData={reader} loop={true} />
         </div>
