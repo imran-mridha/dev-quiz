19f49e3daf46a0a91dfbb181df0d72a81ec32029
MD IMRAN MRIDHA <mdimranmridha.pro@gmail.com>
2022-10-11 23:44:46 +0600

    Update Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,15 +5,15 @@ import reader from '../../assets/quiz-program.json'
 const Hero = () => {
   return (
     <div>
-      <div className=' grid grid-cols-1 lg:grid-cols-2 mx-10 my-10'>
+      <div className=' grid grid-cols-1 lg:grid-cols-2 mx-10 lg:mx-32 my-10'>
         <div className='flex flex-col  justify-center'>
           <div>
             <h2 className='text-4xl text-white font-semibold uppercase'>WellCome To Dev<span className='text-violet-500'>Quiz</span> </h2>
-            <p className='py-5 text-white text-xl'>Dev Quiz provides challenges for several different topics such as React, Javascript, CSS, Git and more. You can test your knowledge directly online.</p>
+            <p className='py-5 text-white text-xl w-full lg:w-9/12'>Dev Quiz provides challenges for several different topics such as React, Javascript, CSS, Git and more. You can test your knowledge directly online.</p>
           </div>
         </div>
         <div className='flex justify-end'>
-          <Lottie className='w-96' animationData={reader} loop={true} />
+          <Lottie animationData={reader} loop={true} />
         </div>
       </div>
     </div>
